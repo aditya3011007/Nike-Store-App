e01ff3597fe7832c67b2ffa363c765b2779b33d4
aditya3011007 <108451255+aditya3011007@users.noreply.github.com>
2022-08-02 23:02:55 +0530

    Add files via upload

---
+++ nike_online_store/lib/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Color kPurpleCustom = Color(0xFF1E0558);
+Color kDiscountColor = Color(0xFFEF5B5C);
+
+LinearGradient kGradient = LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  List: [
+    Color(0xFF793AEC),
+    Color(0xFF6217BC),
+  ],
+);
+
+LinearGradient kGradientGrey = LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  List: [
+    Colors.grey,
+    Colors.grey,
+  ],
+);
